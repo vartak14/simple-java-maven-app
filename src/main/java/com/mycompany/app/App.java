a49da172d03961b7e5e8fd185ca5132785159310
vartak14 <vartakbhagat@gmail.com>
2024-11-08 15:38:50 +0530

    test message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! vartak bhagat";
+    private static final String MESSAGE = "Hello World! test message";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! vartak bhagat", app.getMessage());
+        assertEquals("Hello World! test message", app.getMessage());
     }
 }
