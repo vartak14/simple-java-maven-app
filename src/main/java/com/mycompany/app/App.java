03b750a4de07767ac6a79d74493e1c6955935c48
vartak14 <vartakbhagat@gmail.com>
2024-11-29 13:48:41 +0530

    nothing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! test  hellow VARTAK BHAGAT MBP" ;
+    private static final String MESSAGE = "Hello World! test  hellow VARTAK BHAGAT " ;
 
     public App() {}
 
